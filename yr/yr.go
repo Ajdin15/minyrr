d52f8f031704dc5038abd6c339530e7f484f39f1
Ajdin15 <112623616+Ajdin15@users.noreply.github.com>
2023-04-22 15:59:58 +0200

    Add files via upload

---
+++ yr/yr.go
@@ -0,0 +1,42 @@
+package yr
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+	"errors"
+	"github.com/Ajdin15/funtemps/conv"
+)
+
+func CelsiusToFarhenheitString(celsius string) (string, error) {
+	var fahrFloat float64
+	var err error
+	if celsiusFloat, err := strconv.ParseFloat(celsius, 64); err == nil {
+		fahrFloat = conv.CelsiusToFarhenheit(celsiusFloat)
+	}
+	fahrString := fmt.Sprintf("%.1f", fahrFloat)
+	return fahrString, err
+}
+
+// Forutsetter at vi kjenner strukturen i filen og denne implementasjon 
+// er kun for filer som inneholder linjer hvor det fjerde element
+// på linjen er verdien for temperaturaaling i grader celsius
+func CelsiusToFarhenheitLine(line string) (string, error) {
+
+        dividedString := strings.Split(line, ";")
+	var err error
+	
+	if (len(dividedString) == 4) {
+		dividedString[3], err = CelsiusToFarhenheitString(dividedString[3])
+		if err != nil {
+			return "", err
+		}
+	} else {
+		return "", errors.New("linje har ikke forventet format")
+	}
+	return strings.Join(dividedString, ";"), nil
+	
+	
+	return "Kjevik;SN39040;18.03.2022 01:50;42.8", err
+}
+
